204a541d2929aa340d8d21110b62ab48a7ec0a07
tako <rina.m.takase@gmail.com>
2018-01-29 14:15:12 +0900

    Tweak scss

--- src/containers/Ingredient.jsx
+++ src/containers/Ingredient.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import styles from './Step.scss';
+import styles from './Ingredient.scss';
 
 class Ingredient extends React.Component {
   render() {
