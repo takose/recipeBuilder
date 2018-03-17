617c7be6c05bfac672731f9fd436a019c3bd294e
tako <rina.m.takase@gmail.com>
2018-03-17 10:39:14 +0900

    Add webchoco ui

--- src/index.jsx
+++ src/index.jsx
@@ -12,6 +12,7 @@ import App from './components/App';
 import FF from './components/FF';
 import Player from './components/Player';
 import Settings from './components/Settings';
+import WebChoco from './components/WebChoco';
 
 const history = createHistory();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
@@ -43,6 +44,7 @@ const Root = () => (
     <Route path="/ff" component={FF} />
     <Route path="/player" component={Player} />
     <Route path="/settings" component={Settings} />
+    <Route path="/webChoco" component={WebChoco} />
   </ConnectedSwitch>
 );
 
